0d985a3ba15b7a7ce3e1d59f3e3300bfdfc5ff7f
Jared Hanson <jaredhanson@gmail.com>
2015-11-12 12:46:29 -0800

    Start portin Rakefile to bash.

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# TODO: Replace Rakefile with this
+echo "Hello"
+
+
+for file in *
+do
+  echo "Processing $file file..."
+  # take action on each file. $f store current file name
+  #cat $f
+done
